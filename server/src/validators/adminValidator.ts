eb0e007a7fb6d2750430d26e266d30bf097143fa
Mr Swift <amirshayan1381@yahoo.com>
2026-07-28 20:58:18 +0330

    fix

--- server/src/validators/adminValidator.ts
+++ server/src/validators/adminValidator.ts
@@ -5,5 +5,5 @@ export const createUserSchema = z.object({
   name: z.string().min(2, 'Name must be at least 2 characters'),
   email: z.string().email('Invalid email'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
-  role: z.enum(['admin', 'restaurant_owner', 'customer']).default('restaurant_owner'),
+  role: z.enum(['admin', 'r_owner', 'customer']).default('r_owner'),
 });
